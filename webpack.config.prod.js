e6359a5329e4cd62a775d6a59b5aa97bf4c8a10c
Jafar Aziz <jhaziz747@gmail.com>
2020-08-22 11:11:49 +0700

    fix production build

---
+++ webpack.config.prod.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  devtool: '',
+  resolve: {
+    alias: {
+        components: path.resolve(__dirname, 'src/components'),
+        shared: path.resolve(__dirname, 'src/shared'),
+        helpers: path.resolve(__dirname, 'src/helpers'),
+        icons: path.resolve(__dirname, 'src/icons')
+    },
+    extensions: ['.js']
+  }
+};
